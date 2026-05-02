f61fdc85d8c7b141f30b0e989e15040b8387e165
Cohenz-Musaasizi <imacrecordz@gmail.com>
2026-05-02 21:32:19 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['256756803655'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Marvin Musaasiz', 'Professor'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
+    botName: 'Cohenz Pro Bot',
     prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
@@ -16,7 +16,7 @@ module.exports = {
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot Mini',
+    packname: 'Cohenz Pro Bot',
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
@@ -74,16 +74,15 @@ module.exports = {
     },
     
     // Timezone
-    timezone: 'Asia/Kolkata',
+    timezone: 'Africa/Kampala',
     
     // Limits
     maxWarnings: 3,
     
     // Social Links (optional)
     social: {
-      github: 'https://github.com/mruniquehacker',
-      instagram: 'https://instagram.com/yourusername',
-      youtube: 'http://youtube.com/@mr_unique_hacker'
+      github: 'https://github.com/cohenz-musaasizi',
+      youtube: 'http://youtube.com/@cohenzpro'
     }
 };
   
